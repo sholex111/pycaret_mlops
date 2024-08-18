19a99dfdd15695cdb3efd26c09af09bffcb7dc2f
olusola <sholex111@gmail.com>
2024-08-18 23:32:06 +0100

    first pycaret commit

---
+++ insurance1.py
@@ -0,0 +1,8 @@
+#Insurance ml prediction using insurance data from pycaret repository
+
+# Import dataset from pycaret repository
+pip install pycaret
+from pycaret.datasets import get_data
+insurance = get_data('insurance')
+
+
